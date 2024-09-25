eccba1f7d11960a5d952f3302f65bc88cb614235
Кесель Андрей Николаевич <andrey.kesel@service-it.by>
2024-09-25 17:04:47 +0300

    icon app

--- lib/audio/audio_state.dart
+++
@@ -1,8 +0,0 @@
-import 'package:audio_service/audio_service.dart';
-
-class AudioState {
-  final MediaItem? mediaItem;
-  final Duration position;
-
-  AudioState(this.mediaItem, this.position);
-}
